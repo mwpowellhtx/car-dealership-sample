07fce6512969327b79eab6716182314d625ec7d5
Michael W Powell <mwpowellhtx@gmail.com>
2017-02-12 21:22:42 -0500

    added core data access layer support

---
+++ src/Powell.Data.Access/IRepository.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace Powell.Data
+{
+    using static IsolationLevel;
+    using static RepositoryCacheMode;
+    using static RepositoryFlushMode;
+
+    public interface IRepository : IDisposable
+    {
+        /// <summary>
+        /// Returns the item matching the id.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        T Get<T>(object id)
+            where T : class, new();
+
+        /// <summary>
+        /// Tries to get the item matching the id.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="id"></param>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        bool TryGet<T>(object id, out T item)
+            where T : class, new();
+
+        /// <summary>
+        /// Saves the items into the Repository.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="items"></param>
+        void Save<T>(params T[] items)
+            where T : class, new();
+
+        /// <summary>
+        /// Saves or updates the items into the Repository.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="items"></param>
+        void SaveOrUpdate<T>(params T[] items)
+            where T : class, new();
+
+        /// <summary>
+        /// Deletes the items from the Repository.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="items"></param>
+        void Delete<T>(params T[] items)
+            where T : class, new();
+
+        /// <summary>
+        /// Queries the Repository.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="cacheMode"></param>
+        /// <returns></returns>
+        IQueryable<T> Query<T>(RepositoryCacheMode cacheMode = Ignore)
+            where T : class, new();
+
+        /// <summary>
+        /// Queries the Repository.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="predicate"></param>
+        /// <param name="cacheMode"></param>
+        /// <returns></returns>
+        IEnumerable<T> Query<T>(Func<T, bool> predicate, RepositoryCacheMode cacheMode = Ignore)
+            where T : class, new();
+
+        /// <summary>
+        /// Tries to perform the query.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="result"></param>
+        /// <param name="predicate"></param>
+        /// <param name="cacheMode"></param>
+        /// <returns></returns>
+        bool TryQuery<T>(out IEnumerable<T> result, Func<T, bool> predicate = null, RepositoryCacheMode cacheMode = Ignore)
+            where T : class, new();
+
+        void Transact(Action<IRepository> work,
+            IsolationLevel isolationLevel = ReadCommitted,
+            RepositoryFlushMode flushMode = Commit);
+
+        TResult Transact<TResult>(Func<IRepository, TResult> work, IsolationLevel isolationLevel = ReadCommitted);
+    }
+
+    public interface IRepository<TSession> : IRepository
+    {
+    }
+}
---
+++ src/Powell.Data.Access/RepositoryBase.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace Powell.Data
+{
+    using static IsolationLevel;
+    using static RepositoryCacheMode;
+    using static RepositoryFlushMode;
+
+    public abstract class RepositoryBase<TSession, TProvider> : IRepository<TSession>
+        where TProvider : IRepositorySessionProvider<TSession>
+    {
+        protected internal const RepositoryCacheMode DefaultCacheMode = Ignore;
+
+        protected TSession Session { get; }
+
+        protected RepositoryBase(TProvider provider)
+        {
+            Session = provider.Session;
+        }
+
+        public abstract T Get<T>(object id)
+            where T : class, new();
+
+        public virtual bool TryGet<T>(object id, out T item)
+            where T : class, new()
+            => (item = Get<T>(id)) != null;
+
+        public abstract void Save<T>(params T[] items)
+            where T : class, new();
+
+        public abstract void SaveOrUpdate<T>(params T[] items)
+            where T : class, new();
+
+        public abstract void Delete<T>(params T[] items)
+            where T : class, new();
+
+        public abstract IQueryable<T> Query<T>(RepositoryCacheMode cacheMode = DefaultCacheMode)
+            where T : class, new();
+
+        public virtual IEnumerable<T> Query<T>(Func<T, bool> predicate,
+            RepositoryCacheMode cacheMode = DefaultCacheMode)
+            where T : class, new()
+            => from x in Query<T>(cacheMode) where predicate(x) select x;
+
+        private static bool DefaultPredicate<T>(T item) => true;
+
+        public virtual bool TryQuery<T>(out IEnumerable<T> result, Func<T, bool> predicate = null,
+            RepositoryCacheMode cacheMode = DefaultCacheMode)
+            where T : class, new()
+        {
+            result = Query(predicate ?? DefaultPredicate, cacheMode);
+            return result != null && result.Any();
+        }
+
+        public abstract void Transact(Action<IRepository> work,
+            IsolationLevel isolationLevel = ReadCommitted, RepositoryFlushMode flushMode = Commit);
+
+        public abstract TResult Transact<TResult>(Func<IRepository, TResult> work,
+            IsolationLevel isolationLevel = ReadCommitted);
+
+        protected bool IsDisposed { get; private set; }
+
+        protected virtual void Dispose(bool disposing)
+        {
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            IsDisposed = true;
+        }
+    }
+}
