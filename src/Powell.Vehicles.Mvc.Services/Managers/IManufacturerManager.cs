88c1be04d82dba45c1a9036a2b1e6e964c713d0b
Michael W Powell <mwpowellhtx@gmail.com>
2017-02-14 15:07:51 -0500

    added required view models auto mappings and managers

---
+++ src/Powell.Vehicles.Mvc.Services/Managers/IManufacturerManager.cs
@@ -0,0 +1,6 @@
+namespace Powell.Vehicles.Managers
+{
+    public interface IManufacturerManager : IHibernateManagerBase
+    {
+    }
+}
---
+++ src/Powell.Vehicles.Mvc.ViewModels/Mvc/Manufacturer/ManufacturerViewModels.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Powell.Vehicles.Mvc.Manufacturer
+{
+    public class ManufacturerViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
