4b36bd383a9fc60f76ad15c09ac875e8cd3b8754
Michael W Powell <mwpowellhtx@gmail.com>
2017-02-15 16:35:07 -0500

    tried to get routing to work but it did not work quite like expected for now will not worry about that

--- src/Powell.Vehicles.Mvc/App_Start/RouteConfig.cs
+++ src/Powell.Vehicles.Mvc/App_Start/RouteConfig.cs
@@ -1,22 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Powell.Vehicles
 {
+    using static UrlParameter;
+
     public class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //// TODO: TBD: ? doesn't seem to work properly? i.e. ~/Model/Get/Available/Years/... no joy...
+            // routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new {controller = "Home", action = "Index", id = Optional}
             );
         }
     }
